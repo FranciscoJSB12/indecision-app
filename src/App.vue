5ec3dab3cf2bfa3c2d4c024f7214888e3fc7e209
Francisco Salazar <francisco.salazar@itsense.com.co>
2025-05-04 10:30:10 -0400

    feat: [agregar] contador

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <h1>Mi primera app</h1>
+  <hr />
+  <MyCounter :value="1" />
+</template>
+
+<script setup lang="ts">
+import MyCounter from './components/MyCounter.vue';
+
+</script>
---
+++ src/components/MyCounter.vue
@@ -0,0 +1,20 @@
+<template>
+  <section>
+    <h3>Counter: {{ counter }}</h3>
+    <h3>Square: {{ squareCounter }}</h3>
+
+    <div>
+      <button class="p-5 bg-blue-500 rounded hover:bg-blue-700 mr-2" @click="counter++">+1</button>
+
+      <button class="p-5 bg-blue-500 rounded hover:bg-blue-700 mr-2" @click="counter--">-1</button>
+    </div>
+  </section>
+</template>
+
+<script setup lang="ts">
+import { useCounter } from '@/composables/useCounter';
+
+const props = defineProps<{ value: number }>();
+
+const { counter, squareCounter } = useCounter(props.value);
+</script>
